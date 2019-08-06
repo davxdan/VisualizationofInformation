f8254f6c5c5cbb5831fe9a260f16225862645365
Daniel Davieau <danieldavieau@hotmail.com>
2019-08-05 22:24:53 -0400

    werk

--- dDavieau_Final3DProjectinJva/src/dDavieau_Final3DProjectinJva/dDavieau_Final.java
+++ dDavieau_Final3DProjectinJva/src/dDavieau_Final3DProjectinJva/dDavieau_Final.java
@@ -1,65 +1,50 @@
 package dDavieau_Final3DProjectinJva;
 
 import processing.core.*;
-import processing.data.*;
-import processing.event.*;
-import processing.opengl.*;
-
-import processing.core.*;
-
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
 
 public class dDavieau_Final extends PApplet {
-
 	PVector[] vecData = new PVector[1500];
+
 	int dimensions = 750;
 	Dataloader acousticSignal;
 	int[] y;
 	float x;
 
 	public void setup() {
-
 		acousticSignal = new Dataloader(this, "seg_00a37e.csv", "header", y);
-		
 		for (int i = 0; i < vecData.length; i++, x += .5f) {
 			vecData[i] = new PVector(x, acousticSignal.ydata[i] * 10 + dimensions / 2, x);
 		}
-
 	}
 
 	public void draw() {
+		
 		background(13, 2, 8);
-		translate(width / 2, height / 2);
-		scale(1, -1, 1);
-		rotateY(radians(frameCount));
-
+		translate(width / 2, height / 2, 250);
+		scale(1, -1, 1); // so Y is up, which makes more sense in plotting
+		rotateY(radians(frameCount)/8);
+//		rotateX(radians(frameCount)/4);
+//		rotateZ(noise(radians(frameCount)));
 		noFill();
 		strokeWeight(1);
 
 		translate(-dimensions / 2, -dimensions / 2, -dimensions / 2);
 		for (int i = 0; i < vecData.length; i++) {
-			vecData[i].z = noise(frameCount * 0.005f + i) * dimensions;
+			vecData[i].z = noise(frameCount * 0.001f + i) * dimensions / 3;
 			PVector v = vecData[i];
-			stroke(0, 255, 65, 50);
+			stroke(0, 255, 65, v.y/6);
 			strokeWeight(2);
-			line(v.x, 0, v.z, v.x, v.y, v.z);
+			line(v.x, v.y + 50, v.z, v.x, v.y - 50, v.z);
 			stroke(0, 255, 65);
-			strokeWeight(2);
+			strokeWeight(v.y/60);
 			point(v.x, v.y, v.z);
 
 		}
-
 	}
 
 	public void settings() {
-		size(1500, 1500, P3D);
+		size(1920, 1080, P3D);
+//		fullScreen(P3D);
 	}
 
 	static public void main(String[] passedArgs) {
--- ddavieau_FInal/ddavieau_FInal.pde
+++ ddavieau_FInal/ddavieau_FInal.pde
@@ -6,12 +6,13 @@ int[] y;
 float x;
 
 void setup() {
-  size(1500, 1500, P3D);
-  
+  //size(2500, 2500, P3D);
+  fullScreen(P3D);
+
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   acousticSignal = new Dataloader(this, "seg_00a37e.csv", "header", y); //instantiate a Dataloader object with name acousticSignal
-  
+
   for (int i=0; i<vecData.length; i++, x+=.5f) {
     vecData[i] = new PVector(x, acousticSignal.ydata[i]*10+dimensions/2, x);
   }
@@ -19,25 +20,26 @@ void setup() {
 }
 
 void draw() {
-  background(13,2,8);
-  translate(width/2, height/2);
+  background(13, 2, 8);
+  translate(width/2, height/2, 750);
   scale(1, -1, 1); // so Y is up, which makes more sense in plotting
   rotateY(radians(frameCount));
-
+  //rotateX(radians(frameCount));
+  //rotateZ(noise(radians(frameCount)));
   noFill();
   strokeWeight(1);
 
-  
+
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   translate(-dimensions/2, -dimensions/2, -dimensions/2);
   for (int i=0; i<vecData.length; i++) {
-  vecData[i].z = noise(frameCount * 0.005f + i) * dimensions;
+    vecData[i].z = noise(frameCount * 0.001f + i) * dimensions/2;
     PVector v = vecData[i];
-    stroke(0, 255, 65,50);
+    stroke(0, 255, 65, 25);
     strokeWeight(2);
-    line(v.x, 0, v.z, v.x, v.y, v.z);
+    line(v.x, v.y+100, v.z, v.x, v.y-100, v.z);
     stroke(0, 255, 65);
-    strokeWeight(2);
+    strokeWeight(5);
     point(v.x, v.y, v.z);
     //vertex(v.x, v.z, v.x, v.y, v.z);
   }
